7b66373eea0c3c280dde1fce6464f61bc7830785
junh_eee(이준희) <junh_eee@naver.com>
2023-11-09 18:23:58 +0900

    댓글 대댓글 컴포넌트 분리 작업

--- src/components/board/BoardReply.tsx
+++ src/components/board/BoardReply.tsx
@@ -1,212 +1,22 @@
-import { useState } from 'react';
 import { Row, Col, Card, Form, Button } from 'react-bootstrap';
+import Reply from './reply/Reply';
 
 export default function BoardReply() {
-  const [reReplyEnterCnt, setReReplyEnterCnt] = useState<number>(0);
   return (
     <>
-      <Row>
-        <Col>
-          <Card>
-            <div style={{ padding: '8px 12px 0px 12px' }}>
-              <div
-                id='user'
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  margin: '10px 5px'
-                }}
-              >
-                <span style={{ color: '#3472F7' }}>닉네임</span>
-                <span style={{ margin: '0px 10px', color: '#e3e3e3' }}>|</span>
-                <span style={{ fontSize: '13px', color: '#999999' }}>시간</span>
+      <div id='replys'>
+        <Row>
+          <Col>
+            <Card>
+              <div style={{ padding: '10px 20px' }}>
+                <Reply type='first' />
+                <Reply type='rere' />
+                <Reply type='last' />
               </div>
-
-              <div id='reply' style={{ paddingLeft: '15px' }}>
-                <span style={{ color: '#666666' }}>
-                  댓글입니다 댓글입니다 댓글입니다
-                </span>
-                <div style={{ marginTop: '5px' }}>
-                  <span
-                    style={{
-                      fontSize: '13px',
-                      border: '1px solid #e3e3e3',
-                      padding: '2px'
-                    }}
-                  >
-                    답글
-                  </span>
-                </div>
-              </div>
-
-              <div id='br' style={{ padding: '10px 10px 0px 10px' }}>
-                <div style={{ borderBottom: '1px solid #e3e3e3' }}></div>
-              </div>
-            </div>
-
-            <div style={{ padding: '8px 12px 0px 12px' }}>
-              <div
-                id='user'
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  margin: '10px 5px'
-                }}
-              >
-                <span style={{ color: '#3472F7' }}>닉네임</span>
-                <span style={{ margin: '0px 10px', color: '#e3e3e3' }}>|</span>
-                <span style={{ fontSize: '13px', color: '#999999' }}>시간</span>
-              </div>
-
-              <div id='reply' style={{ paddingLeft: '15px' }}>
-                <span style={{ color: '#666666' }}>
-                  댓글입니다 댓글입니다 댓글입니다
-                </span>
-                <div style={{ marginTop: '5px' }}>
-                  <span
-                    style={{
-                      fontSize: '13px',
-                      border: '1px solid #e3e3e3',
-                      padding: '2px'
-                    }}
-                  >
-                    답글
-                  </span>
-                  {/* 답글 다는 영역 */}
-                  <div>
-                    <Form.Group>
-                      <Form.Control
-                        style={{
-                          margin: '8px 0px',
-                          border: '1px solid #e3e3e3',
-                          borderRadius: '4px',
-                          padding: '5px 10px',
-                          outline: 'none',
-                          resize: 'none'
-                        }}
-                        plaintext
-                        cols={80}
-                        placeholder='댓글 입력해조잉!'
-                        as='textarea'
-                        rows={reReplyEnterCnt < 2 ? 1 : reReplyEnterCnt}
-                        onChange={e => {
-                          const cnt =
-                            (e.target.value.match(/\n/g) || []).length + 1;
-                          setReReplyEnterCnt(cnt);
-                        }}
-                      />
-                    </Form.Group>
-                    <Button
-                      className='btn-success btn-fill'
-                      style={{ width: '100%' }}
-                    >
-                      등록
-                    </Button>
-                  </div>
-                  {/* -------- */}
-                  {/* 달린 답글들 */}
-                  <div style={{ display: 'flex' }}>
-                    <div style={{ paddingTop: '18px', marginLeft: '20px' }}>
-                      <div className='font-icon-detail'>
-                        <i className='nc-icon nc-stre-down'></i>
-                      </div>
-                    </div>
-
-                    <div style={{ marginLeft: '0px' }}>
-                      <div style={{ padding: '8px 12px 0px 12px' }}>
-                        <div
-                          id='user'
-                          style={{
-                            display: 'flex',
-                            alignItems: 'center',
-                            margin: '10px 5px'
-                          }}
-                        >
-                          <span style={{ color: '#3472F7' }}>닉네임</span>
-                          <span
-                            style={{ margin: '0px 10px', color: '#e3e3e3' }}
-                          >
-                            |
-                          </span>
-                          <span style={{ fontSize: '13px', color: '#999999' }}>
-                            시간
-                          </span>
-                        </div>
-
-                        <div id='reply' style={{ paddingLeft: '15px' }}>
-                          <span style={{ color: '#666666' }}>
-                            여기 FLEX먹어서 영역 줄어들어 있으니까 ROW Col줘서
-                            넓히던지 방법좀 찾아바라
-                          </span>
-                          <div style={{ marginTop: '5px' }}>
-                            <span
-                              style={{
-                                fontSize: '13px',
-                                border: '1px solid #e3e3e3',
-                                padding: '2px'
-                              }}
-                            >
-                              답글
-                            </span>
-                          </div>
-                        </div>
-
-                        <div id='br' style={{ padding: '10px 10px 0px 10px' }}>
-                          <div
-                            style={{ borderBottom: '1px solid #e3e3e3' }}
-                          ></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* --------------- */}
-                </div>
-              </div>
-
-              <div id='br' style={{ padding: '10px' }}>
-                <div style={{ borderBottom: '1px solid #e3e3e3' }}></div>
-              </div>
-            </div>
-
-            <div style={{ padding: '8px 12px 0px 12px' }}>
-              <div
-                id='user'
-                style={{
-                  display: 'flex',
-                  alignItems: 'center',
-                  margin: '10px 5px'
-                }}
-              >
-                <span style={{ color: '#3472F7' }}>닉네임</span>
-                <span style={{ margin: '0px 10px', color: '#e3e3e3' }}>|</span>
-                <span style={{ fontSize: '13px', color: '#999999' }}>시간</span>
-              </div>
-
-              <div id='reply' style={{ paddingLeft: '15px' }}>
-                <span style={{ color: '#666666' }}>
-                  댓글입니다 댓글입니다 댓글입니다
-                </span>
-                <div style={{ marginTop: '5px' }}>
-                  <span
-                    style={{
-                      fontSize: '13px',
-                      border: '1px solid #e3e3e3',
-                      padding: '2px'
-                    }}
-                  >
-                    답글
-                  </span>
-                </div>
-              </div>
-
-              <div id='br' style={{ padding: '10px' }}>
-                <div style={{ borderBottom: '1px solid #e3e3e3' }}></div>
-              </div>
-            </div>
-          </Card>
-        </Col>
-      </Row>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 }
--- src/containers/board/DetailContainer.tsx
+++ src/containers/board/DetailContainer.tsx
@@ -13,52 +13,44 @@ export default function DetailContainer() {
       <Container fluid>
         <BoardDetail detailNo={no} />
 
-        <>
-          <div style={{ marginBottom: '30px' }}>
-            <Row>
-              <Col>
-                <div style={{ paddingLeft: '10px' }}>
-                  <span style={{ color: '#000000' }}>댓글 39</span>
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col md='10'>
-                <Form.Group>
-                  <Form.Control
-                    // style={{
-                    //   padding: '8px 12px',
-                    //   border: '1px solid #e3e3e3',
-                    //   borderRadius: '4px'
-                    // }}
-                    // plaintext
-                    style={{
-                      resize: 'none',
-                      outline: 'none'
-                    }}
-                    cols={80}
-                    placeholder='댓글 입력해조잉!'
-                    as='textarea'
-                    rows={replyEnterCnt < 4 ? 3 : replyEnterCnt}
-                    onChange={e => {
-                      const cnt =
-                        (e.target.value.match(/\n/g) || []).length + 2;
-                      setReplyEnterCnt(cnt);
-                    }}
-                  />
-                </Form.Group>
-              </Col>
-              <Col md='2'>
-                <Button
-                  className='btn-primary btn-fill'
-                  style={{ width: '100%', height: '100%' }}
-                >
-                  등록
-                </Button>
-              </Col>
-            </Row>
-          </div>
-        </>
+        <div id='replyBtn' style={{ marginBottom: '30px' }}>
+          <Row>
+            <Col>
+              <div style={{ paddingLeft: '10px' }}>
+                <span style={{ color: '#000000' }}>댓글 39</span>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col md='10'>
+              <Form.Group>
+                <Form.Control
+                  style={{
+                    resize: 'none',
+                    outline: 'none'
+                  }}
+                  cols={80}
+                  placeholder='댓글 입력해조잉!'
+                  as='textarea'
+                  rows={replyEnterCnt < 4 ? 3 : replyEnterCnt}
+                  onChange={e => {
+                    const cnt = (e.target.value.match(/\n/g) || []).length + 2;
+                    setReplyEnterCnt(cnt);
+                  }}
+                />
+              </Form.Group>
+            </Col>
+            <Col md='2'>
+              <Button
+                className='btn-primary btn-fill'
+                style={{ width: '100%', height: '100%' }}
+              >
+                등록
+              </Button>
+            </Col>
+          </Row>
+        </div>
+
         <BoardReply />
       </Container>
     </>
